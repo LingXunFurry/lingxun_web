da8e1383955592b367d5bd4f0fa7368b9e3aa384
LingXunFurry <1651388504@qq.com>
2026-06-05 00:38:54 +0800

    更新了画廊界面的跳转逻辑

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 在这里添加你的域名配置
+  site: 'https://lingxun.me', 
+  
   vite: {
     plugins: [tailwindcss()]
   }
